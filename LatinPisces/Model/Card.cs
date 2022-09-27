ce156ffaed5689d1dfbf6b4f1fc576ab32780c24
shlyapp <shlyapp.exe@gmail.com>
2022-09-28 01:29:05 +0400

    Класс модели Card

---
+++ LatinPisces/Model/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinPisces.Model
+{
+    internal class Card
+    {
+        private string _name;
+        private string _translate;
+        private string _transcription;
+        private string _path;
+
+        public Card()
+        {
+
+        }
+
+        public Card(string name, string path, string transalate, string transcription)
+        {
+            this._name = name;
+            this._path = path;
+            this._translate = transalate;
+        }
+
+    }
+}
