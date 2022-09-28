b30b8166e55fad842d3b0bb314041565ec86af0b
shlyapp <shlyapp.exe@gmail.com>
2022-09-28 08:54:04 +0400

    Модель класса и контекст для базы данных

---
+++ LatinPisces/Models/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace LatinPisces.Models
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<Card> Cards { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=pisces.db");
+        }
+    }
+}
