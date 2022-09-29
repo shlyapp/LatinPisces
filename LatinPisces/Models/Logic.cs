55a723342b190f103d5d36ed9d6da8f42bf43a4a
shlyapp <shlyapp.exe@gmail.com>
2022-09-29 21:59:03 +0400

    Реализация логики режима "тест"
    Отображение нужных элементов, случайная генерация ответов и проверка на правильность

---
+++ LatinPisces/Models/Logic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinPisces.Models
+{
+    internal static class Logic
+    {
+        public static List<string> GetRandomAnswers(Card card)
+        {
+            var random = new Random();
+            List<string> answers = card.GetWrongAnswers().OrderBy(s => random.Next()).Take(4).ToList();
+            answers.Add(card.Translation);
+
+            return answers.OrderBy(s => random.Next()).Take(5).ToList();
+        }
+    }
+}
--- LatinPisces/Views/TestPage.xaml.cs
+++ LatinPisces/Views/TestPage.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using LatinPisces.Models;
+
 namespace LatinPisces.View
 {
     /// <summary>
@@ -20,9 +22,59 @@ namespace LatinPisces.View
     /// </summary>
     public partial class TestPage : Page
     {
+
+        private int i = 0;
+
         public TestPage()
         {
             InitializeComponent();
+            LoadTest();
+        }
+
+        private void LoadTest()
+        {
+            RefreshButtons();
+        }
+
+        private void GiveAnswer(object sender, RoutedEventArgs e)
+        {
+            
+
+            Button button = sender as Button;
+
+            if (button.Content == Data.GetCards()[i].Translation)
+            {
+                MessageBox.Show("Верный ответ!");
+            }
+            else
+            {
+                MessageBox.Show("Неверный ответ!");
+            }
+
+            i++;
+            if (i == Data.GetCards().Count)
+            {
+                NavigationService.GoBack();
+            }
+            else
+            {
+                RefreshButtons();
+            }
+            
+        }
+
+        private void RefreshButtons()
+        {
+            CardImage.Source = BitmapFrame.Create(new Uri(Data.GetCards()[i].Path));
+
+            List<string> asnwers = Logic.GetRandomAnswers(Data.GetCards()[i]);
+            IEnumerable<Button> collection = grid.Children.OfType<Button>();
+            int j = 0;
+            foreach (Button button in collection)
+            {
+                button.Content = asnwers[j];
+                j++;
+            }
         }
     }
 }
